bf302351e9db6aa1c05bee3cc2c1538f82e340b5
Joseph Townshend <joetownshend@mac.com>
2020-04-10 09:43:40 +0100

    variables

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,8 +2,12 @@ public class Hello {
 
   static String name = "Joseph";
 
+  static int myTotal = 1000;
+
+  static int myLastOne = myTotal -1000;
+
   public static void main(String[] args) {
-    System.out.println("Hello, " + name);
+    System.out.println(myLastOne);
   }
 }
 
