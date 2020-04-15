f6d19434fbca0e620bdeab5d0e24383452bb15d4
Joseph Townshend <joetownshend@mac.com>
2020-04-15 08:21:39 +0100

    tidy up with notes

--- ByteShortIntLong/src/Main.java
+++ ByteShortIntLong/src/Main.java
@@ -4,6 +4,22 @@ public class Main {
 
     // this demonstrates the overflow and underflow of different data types in java.
 
+    // bit size is also called width - this is how much space they take up
+    // not how many decimal places they occupy.
+
+    // 8 bits
+    byte myMinByteValue = Byte.MIN_VALUE;
+    byte myMaxByteValue = Byte.MAX_VALUE;
+    System.out.println("Byte Minimum Value = " + myMinByteValue);
+    System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+    // 16 bits
+    short myMinShortValue = Short.MIN_VALUE;
+    short myMaxShortValue = Short.MAX_VALUE;
+    System.out.println("Byte Short Value = " + myMinShortValue);
+    System.out.println("Byte Short Value = " + myMaxShortValue);
+
+    // 32 bits
     int myMinIntValue = Integer.MIN_VALUE;
     int myMaxIntValue = Integer.MAX_VALUE;
     System.out.println("Iteger Minimum Value = " + myMinIntValue);
@@ -11,21 +27,11 @@ public class Main {
     System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
     System.out.println("Busted MIN value = " + (myMinIntValue + 1));
 
-    byte myMinByteValue = Byte.MIN_VALUE;
-    byte myMaxByteValue = Byte.MAX_VALUE;
-    System.out.println("Byte Minimum Value = " + myMinByteValue);
-    System.out.println("Byte Maximum Value = " + myMaxByteValue);
-
-    short myMinShortValue = Short.MIN_VALUE;
-    short myMaxShortValue = Short.MAX_VALUE;
-    System.out.println("Byte Short Value = " + myMinShortValue);
-    System.out.println("Byte Short Value = " + myMaxShortValue);
-
+    // 64 bits
     long myMinLongValue = Long.MIN_VALUE;
     long myMaxLongValue = Long.MAX_VALUE;
     System.out.println("Byte Long Value = " + myMinLongValue);
     System.out.println("Byte Long Value = " + myMaxLongValue);
-
 
   }
 }
