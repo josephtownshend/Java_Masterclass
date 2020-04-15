48137cfc5e1d60fc459a1d78327f5370a9a24f9d
Joseph Townshend <joetownshend@mac.com>
2020-04-15 08:09:34 +0100

    byte short long demo

--- ByteShortIntLong/src/Main.java
+++ ByteShortIntLong/src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
-  public static void Main(String[] args) {
+  public static void main(String[] args) {
+
+    int myMinIntValue = Integer.MIN_VALUE;
+    int myMaxIntValue = Integer.MAX_VALUE;
+    System.out.println("Iteger Minimum Value = " + myMinIntValue);
+    System.out.println("Iteger Maximum Value = " + myMaxIntValue);
+    System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
+    System.out.println("Busted MIN value = " + (myMinIntValue + 1));
 
   }
 }
