fb56a93978a6282aa4791bb3372bece6a20e44cd
Joseph Townshend <joetownshend@mac.com>
2020-04-16 08:59:02 +0100

    floats and doubles challenge

--- FloatAndDouble/src/Main.java
+++ FloatAndDouble/src/Main.java
@@ -11,6 +11,21 @@ public class Main {
     System.out.println("Double minimum value = " + myMinDoubleValue);
     System.out.println("Double maximum value = " + myMaxDoubleValue);
 
+    int myIntValue = 5 / 3;
+    float myFloatValue = (float) 5 / 3;
+    float myFloatValue2 = 5.52f / 3f;
+    double myDoubleValue = 5d / 3d;
+
+    System.out.println("My int value = " + myIntValue);
+    System.out.println("My float value = " + myFloatValue);
+    System.out.println("My float value 2 = " + myFloatValue2);
+    System.out.println("My double value = " + myDoubleValue);
+
+    // Challenge - lbs to kg converter
+    int numberOfPounds = 2;
+    double numberOfKilos = numberOfPounds * 0.45359237;
+    System.out.println(numberOfKilos);
+
   }
 }
 
