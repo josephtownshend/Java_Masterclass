335ce32c27582941f260f327ab0134df63d80f76
Joseph Townshend <joetownshend@mac.com>
2020-04-16 09:30:07 +0100

    booleans and chars

---
+++ BooleansAndChars/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    char myChar = 'D';
+    char myUnicodeChar = '\u0044';
+    char myUnicodeChar2 = '\u00A9';
+    System.out.println(myChar);
+    System.out.println(myUnicodeChar);
+    System.out.println(myUnicodeChar2);
+
+    boolean mytrueBooleanValue = true;
+    boolean myfalseBooleanValue = false;
+
+  }
+
+}
+
