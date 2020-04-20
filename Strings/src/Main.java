4585e7ccd39feca23868f3c34c65103ab70f6fdf
Joseph Townshend <joetownshend@mac.com>
2020-04-20 08:40:39 +0100

    Strings

---
+++ Strings/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+  public static void main(String[] args) {
+    // byte
+    // short
+    // int
+    // long
+    // float
+    // double
+    // char
+    // boolean
+
+    String myString = "This is a string";
+    System.out.println("myString is equal to " + myString);
+    myString = myString + ", and this is more.";
+    System.out.println("myString is equal to " + myString);
+
+  }
+
+}
+
