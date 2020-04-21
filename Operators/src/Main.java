378729c22d20868a869ef75aed19854072fe9e11
Joseph Townshend <joetownshend@mac.com>
2020-04-21 08:34:37 +0100

    Update Main.java

--- Operators/src/Main.java
+++ Operators/src/Main.java
@@ -8,8 +8,38 @@ public class Main {
 
     int previousResult = result;
     System.out.println(previousResult);
+
     result = result - 1;
     System.out.println(result);
+
+    result = result * 10;
+    System.out.println(result);
+
+    result = result / 5;
+    System.out.println(result);
+
+    result = result % 3;
+    System.out.println(result);
+
+    result++;
+    System.out.println(result);
+
+    result--;
+    System.out.println(result);
+
+    result += 2;
+    System.out.println(result);
+
+    result -= 1;
+    System.out.println(result);
+
+    result *= 5;
+    System.out.println(result);
+
+    result /= 2;
+    System.out.println(result);
+
+
   }
 
 }
