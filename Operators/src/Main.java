6572daa4c8d1d9e08168f835d85b1aec987cf49a
Joseph Townshend <joetownshend@mac.com>
2020-04-21 08:28:29 +0100

    operators, operands and expressions

---
+++ Operators/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    int result = 1 + 2; // 1 + 2 = 3
+    System.out.println(result);
+
+    int previousResult = result;
+    System.out.println(previousResult);
+    result = result - 1;
+    System.out.println(result);
+  }
+
+}
+
