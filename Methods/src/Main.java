aa05d9d85e0cc86959c4f340a29da59ff26cabda
Joseph Townshend <joetownshend@mac.com>
2020-04-22 09:39:10 +0100

    update methods

--- Methods/src/Main.java
+++ Methods/src/Main.java
@@ -2,35 +2,18 @@
 public class Main {
 
   public static void main(String[] args) {
-    boolean gameOver = true;
-    int score = 800;
-    int levelCompleted = 5;
-    int bonus = 100;
 
-    calculatorScore();
-
-    score = 10000;
-    levelCompleted = 8;
-    bonus = 200;
-
-    if (gameOver) {
-      int finalScore = score + levelCompleted * bonus;
-      System.out.println("Your other final score was " + finalScore);
-    }
+    calculatorScore(true, 800, 5, 100);
+    calculatorScore(true, 10000, 8, 200);
   }
 
-  public static void calculatorScore() {
-    boolean gameOver = true;
-    int score = 800;
-    int levelCompleted = 5;
-    int bonus = 100;
+  public static void calculatorScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
     if (gameOver) {
       int finalScore = score + levelCompleted * bonus;
       finalScore += 1000;
       System.out.println("Your final score was " + finalScore);
     }
-
   }
 
 }
