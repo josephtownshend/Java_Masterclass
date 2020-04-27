7325a8cfcc4043f98314d3591d60f113f8545d13
Joseph Townshend <joetownshend@mac.com>
2020-04-27 09:02:01 +0100

    megaBytesConverter

---
+++ MegaBytesConverter/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    MegaBytesConverter.printMegaBytesConverter(2500);
+
+  }
+
+}
+
---
+++ MegaBytesConverter/src/MegaBytesConverter.java
@@ -0,0 +1,12 @@
+
+public class MegaBytesConverter {
+
+  public static void printMegaBytesConverter (int kiloBytes) {
+
+      float resultUnits = kiloBytes/1024;
+
+      System.out.print(resultUnits);
+
+  }
+}
+
