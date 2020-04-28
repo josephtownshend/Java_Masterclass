01c597a69b08907916f53d77cc610f7a553d0ad4
Joseph Townshend <joetownshend@mac.com>
2020-04-28 09:24:03 +0100

    barkingDog challenge sollution

---
+++ BarkingDog/src/barkingDog.java
@@ -0,0 +1,15 @@
+
+public class barkingDog {
+
+  public static boolean shouldWakeUp(boolean isBarking, int hourOfDay) {
+
+    if (hourOfDay < 0 || hourOfDay > 23) {
+      return false;
+    } else if (isBarking && hourOfDay < 8 || hourOfDay > 22) {
+      return true;
+    } else {
+    return false;
+    }
+  }
+}
+
