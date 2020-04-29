8e853b803c4c16af2b0082aa2ff464d760b1119c
Joseph Townshend <joetownshend@mac.com>
2020-04-29 08:31:23 +0100

    adds tests for leapYearCalc

---
+++ LeapYearCalculator/src/test/TestLeapYearCalculator.java
@@ -0,0 +1,23 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import main.LeapYearCalculator;
+
+public class TestLeapYearCalculator {
+
+  @Test
+  public void shouldCalculateLeapYear() {
+
+    assertEquals(true, LeapYearCalculator.isLeapYear(1600));
+    assertEquals(true, LeapYearCalculator.isLeapYear(2000));
+    assertEquals(true, LeapYearCalculator.isLeapYear(2400));
+
+    assertEquals(false, LeapYearCalculator.isLeapYear(-1600));
+    assertEquals(false, LeapYearCalculator.isLeapYear(2017));
+  }
+
+}
+
