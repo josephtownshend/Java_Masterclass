6c1e5abbc4cc40d9b514cbd5c7b9b9efad2c5411
Joseph Townshend <joetownshend@mac.com>
2020-05-06 08:48:00 +0100

    hasEqualSum complete

---
+++ EqualSumChecker/src/EqualSumChecker.java
@@ -0,0 +1,13 @@
+
+public class EqualSumChecker {
+
+  public static boolean hasEqualSum(int firstNum, int secondNum, int total) {
+
+    if (firstNum + secondNum == total) {
+      return true;
+    }
+    return false;
+
+  }
+}
+
