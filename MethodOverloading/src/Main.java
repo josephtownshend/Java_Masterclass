cad5c96659fe77b93b8fffb9abdd304ff5158e0c
Joseph Townshend <joetownshend@mac.com>
2020-05-12 08:38:12 +0100

    class work complete

--- MethodOverloading/src/Main.java
+++ MethodOverloading/src/Main.java
@@ -5,11 +5,25 @@ public class Main {
     int newScore = calculateScore("Joe", 500);
     System.out.println("New score is " + newScore);
 
+    int secondScore = calculateScore(1000);
+    System.out.println("New score is " + secondScore);
+
+    calculateScore();
   }
 
   public static int calculateScore(String playerName, int score) {
     System.out.println("Player " + playerName + " scored " + score + " points");
     return score * 1000;
+  }
+
+  public static int calculateScore(int score) {
+    System.out.println("Unnamed scored " + score + " points");
+    return score * 1000;
+  }
+
+  public static int calculateScore() {
+    System.out.println("No player");
+    return 0;
   }
 
 }
