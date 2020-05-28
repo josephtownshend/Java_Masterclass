63e87abed154488ba211f0af8dff478a7a861c9d
Joseph Townshend <joetownshend@mac.com>
2020-05-28 09:05:24 +0100

    MinutesToYearsAndDaysCalculator challenge

---
+++ MinutesToYearsAndDaysCalculator/src/MinutesToYearsAndDaysCalculator.java
@@ -0,0 +1,16 @@
+
+public class MinutesToYearsAndDaysCalculator {
+
+  public static void printYearsAndDays(long minutes) {
+    if (minutes < 0) {
+      System.out.println("Invalid Value");
+    } else if (minutes >= 0) {
+
+      long year = minutes / 525600;
+      long remainingMinutes = minutes % 525600;
+      long day = remainingMinutes / 1440;
+      System.out.println(minutes + " min = " + year + " y and " + day + " d");
+    }
+  }
+
+}
