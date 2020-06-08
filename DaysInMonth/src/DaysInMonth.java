740c978437252febd4be174a156a289313841b4e
Joseph Townshend <joetownshend@mac.com>
2020-06-08 08:50:09 +0100

    isLeapYear solution
    Loved the solution to this - nested if, and an if/else in a switch statement.

--- DaysInMonth/src/DaysInMonth.java
+++ DaysInMonth/src/DaysInMonth.java
@@ -1,38 +1,50 @@
 
 public class DaysInMonth {
 
-  public static boolean isLeapyear(int year) {
+  public static boolean isLeapYear(int year) {
 
-    if(year >= 1 && year <= 9999) {
-      return false;
-    } else if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
-      return true;
-    } else {
-      return false;
+    if (year >= 1 && year <= 9999) {
+
+      if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+        return true;
+      } else {
+        return false;
+      }
     }
+    return false;
   }
+
 
   public static int getDaysInMonth(int month, int year) {
 
-    if(month < 1 || month > 12) {
+    if (month < 1 || month > 12) {
       return -1;
-    } else if(year < 1 || year > 9999) {
+    } else if (year < 1 || year > 9999) {
       return -1;
     } else {
 
-      switch(month) {
-        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+      switch (month) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
           return 31;
         case 2:
-          return 29;
-        case 4: case 6: case 9: case 11:
+          if (isLeapYear(year))
+            return 29;
+          else
+            return 28;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
           return 30;
-
       }
     }
     return year;
   }
 
-
 }
-
