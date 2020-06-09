0d53cbe1e77f0647efe47b6734c18cd8e9f52e09
Joseph Townshend <joetownshend@mac.com>
2020-06-09 08:27:28 +0100

    forLoop complete

--- ForLoops/src/ForLoops.java
+++ ForLoops/src/ForLoops.java
@@ -3,9 +3,15 @@ public class ForLoops {
 
   public static void main(String[] args) {
 
-
-
+    // Ascending
     for(double i = 2.0; i < 9.0; i++) {
+      System.out.println(String.format("%.2f", 10000*(i/100)));
+    }
+
+    System.out.println("*********************");
+
+    // Descending
+    for(double i = 8.0; i > 1.0; i--) {
       System.out.println(String.format("%.2f", 10000*(i/100)));
     }
 
