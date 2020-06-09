2b00a7b0c413713447b54e34ca92a9ed3fe15079
Joseph Townshend <joetownshend@mac.com>
2020-06-09 08:17:41 +0100

    forLoops exercise + %.2f

---
+++ ForLoops/src/ForLoops.java
@@ -0,0 +1,15 @@
+
+public class ForLoops {
+
+  public static void main(String[] args) {
+
+
+
+    for(double i = 2.0; i < 9.0; i++) {
+      System.out.println(String.format("%.2f", 10000*(i/100)));
+    }
+
+  }
+
+}
+
