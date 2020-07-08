39663440eefc9bbe3195cfffb62208a86eaaf3a1
Joseph Townshend <joetownshend@mac.com>
2020-07-08 16:20:05 +0100

    classes part 1

---
+++ ClassesIntroduction/src/Car.java
@@ -0,0 +1,23 @@
+
+public class Car {
+
+  // state of the car
+  private int doors;
+  private int wheels;
+  private String model;
+  private String engine;
+  private String colour;
+
+
+  public int getDoors() {
+    return doors;
+  }
+  public void setDoors(int doors) {
+    this.doors = doors;
+  }
+
+
+
+
+}
+
---
+++ ClassesIntroduction/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Car porsche = new Car();
+    Car holden = new Car();
+
+    porsche.setDoors(5);
+    holden.setDoors(3);
+  }
+
+}
+
